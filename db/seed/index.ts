f206a554f7fbd4b7cd7be6a7167ee4385a1bae55
DmytroLysachenko <dlysachenko98@gmail.com>
2026-06-28 21:41:52 +0200

    prune obsolete db seed helpers

--- db/seed/index.ts
+++ db/seed/index.ts
@@ -1,5 +1,2 @@
-export { seedAllMetadata } from './all-metadata';
 export { seedBondMetadata } from './bond-metadata';
 export { seedSeriesMetadata } from './series-metadata';
-export { seedTaxRules } from './tax-rules';
-export { seedWiborSeriesMetadata } from './wibor-series';
