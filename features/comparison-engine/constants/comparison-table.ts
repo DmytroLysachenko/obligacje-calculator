886a55f8811ee4a2e60158b2e83c40c971f97935
Dmytro Lysachenko <dlysachenko98@gmail.com>
2026-07-31 21:13:52 +0200

    chore: remove unused modules

--- features/comparison-engine/constants/comparison-table.ts
+++
@@ -1,7 +0,0 @@
-import { ComparisonTableGranularity } from '../lib/comparison-table-model';
-
-export const COMPARISON_TABLE_GRANULARITY_OPTIONS: ComparisonTableGranularity[] = [
-  'monthly',
-  'quarterly',
-  'yearly',
-];
