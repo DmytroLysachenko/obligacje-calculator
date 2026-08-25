ced26f1f1033bce386b1cfd63456269ec293cec8
Dmytro Lysachenko <dlysachenko98@gmail.com>
2026-08-25 19:00:00 +0200

    chore(core): remove dead reverse engine

--- features/bond-core/utils/calculations.ts
+++ features/bond-core/utils/calculations.ts
@@ -3,5 +3,4 @@ import { Decimal } from 'decimal.js';
 Decimal.set({ precision: 20, rounding: Decimal.ROUND_HALF_UP });
 
 export { calculateRegularInvestment } from './engine/regular-investment-engine';
-export { calculateReverseBondInvestment } from './engine/reverse-bond-engine';
 export { calculateBondInvestment } from './engine/single-bond-engine';
